d271aec1c1b8323f33dc02e9c91157b50f9e7146
Albert Montufar <amontufar@madcapsoftware.com>
2023-06-05 14:14:56 -0400

    Cleaning up is almost complete. Trying to write in different columns

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 # Used to load sensitive data to the Environment variables.
 from dotenv import load_dotenv
-
-load_dotenv()
-# PrettyPrint to make it easier on the eye
+# PrettyPrint
 import pprint
 
-pp = pprint.PrettyPrinter()
+# Used to write to csv
+import csv
 
+# Load credentials to environment
+load_dotenv()
 auth_manager = SpotifyClientCredentials()
+# Instantiate Spotipy
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 # ID of the playlist
@@ -19,15 +21,32 @@ playlist = 'https://open.spotify.com/playlist/5gSSKO3y0Bziz6V9Sc7egA?si=4a0b64b7
 
 # Will hold the list of tracks
 tracks = []
-result = sp.playlist_items(playlist, fields='items.track.name, items.track.artists.name, items.track.album.name',
+
+# Get playlist items but filter by track name, artist, and album
+result = sp.playlist_items(playlist,
+                           fields='items.track.name, items.track.artists.name',
                            additional_types=['track'])
+# Add results to the list
 tracks.extend(result['items'])
 
-# Ensures all songs are captured.
-# while result['next']:
-#     result = sp.next(result)
-#     tracks.extend(result['items'])
+with open('Wedding Songs.csv', 'w', encoding='UTF8', newline='') as file:
+    header = ['Artist', 'Song']
+    writer = csv.writer(file)
+    writer.writerow(header)
 
-pp.pprint(tracks)
+    for values in tracks:
+        # Convert dictionary items into a string
+        temp = str(values)
+        # Cleanup
+        temp = temp.replace('[', '').replace(']', '').replace('{', '').replace('}', '').replace(':', '').replace("\"",
+                                                                                                                 "")
+        temp = temp.replace('track', '').replace('\'', '').replace('album name', '').replace('artists name',
+                                                                                             '').replace(
+            'name', '')
+        temp = " ".join(temp.split())
+        writer.writerow([temp])
 
-# Test
+pp = pprint.PrettyPrinter()
+
+print(temp)
+print(type(temp))
